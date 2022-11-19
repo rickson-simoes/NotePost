03863244f36214ad29c3df1f036a8248d5aee849
Rickson Simoes <rickson.simoes@hotmail.com>
2022-11-19 15:46:44 -0300

    :wrench: Avatar size/ :wrench: Post content avatar changes

--- src/Components/Avatar/index.tsx
+++ src/Components/Avatar/index.tsx
@@ -3,14 +3,35 @@ import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
 interface IAvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
-  avatarSideBar?: boolean;
+  avatarSideBar?: 1 | 2 | 3;
 }
 
-export function Avatar({ avatarSideBar = true, ...props }: IAvatarProps) {
+export function Avatar({ avatarSideBar = 1, ...props }: IAvatarProps) {
+  //styles.avatarImagePost
+
+  let avatarSize;
+
+  switch (avatarSideBar) {
+    case 1: {
+      avatarSize = styles.avatarLarge
+      break;
+    }
+
+    case 2: {
+      avatarSize = styles.avatarMedium
+      break;
+    }
+
+    case 3: {
+      avatarSize = styles.avatarSmall
+      break;
+    }
+  }
+
 
   return (
     <img
-      className={`${styles.avatar} ${avatarSideBar ? styles.avatarImage : styles.avatarImagePost}`}
+      className={`${styles.avatar} ${avatarSize}`}
       alt="User avatar photo"
       loading="lazy"
       {...props}
--- src/Components/MainContent/index.tsx
+++ src/Components/MainContent/index.tsx
@@ -17,8 +17,81 @@ export function MainContent() {
         <article className={styles.post}>
           <header className={styles.postAuthor}>
             <Avatar
+              src="https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              avatarSideBar={2}
+            />
+            <div className={styles.postAuthorInfo}>
+              <strong>Thomas Garden <span> - Quote Post <Pencil size={12} /></span></strong>
+              <time title="19/11/2022 18:30" dateTime="Tuesday, 19 november 2022">
+                19/11/2022 - 18:30
+              </time>
+            </div>
+          </header>
+
+          <section className={styles.postContent}>
+            This makes sense
+          </section>
+
+          <section className={styles.repostContent}>
+            <header className={styles.postAuthor}>
+              <Avatar
+                src="https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                avatarSideBar={3}
+              />
+              <div className={styles.postAuthorInfo}>
+                <strong>Annie Doe <span>(You)</span></strong>
+                <time title="17/11/2022 16:00" dateTime="Tuesday, 17 november 2022">
+                  17/11/2022 - 16:40
+                </time>
+              </div>
+            </header>
+
+            <div className={styles.repostContentText}>
+              content of the post
+            </div>
+          </section>
+        </article>
+
+
+        <article className={styles.post}>
+          <header className={styles.postAuthor}>
+            <Avatar
+              src="https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              avatarSideBar={2}
+            />
+            <div className={styles.postAuthorInfo}>
+              <strong>Lee Mel <span>- Repost <ShareNetwork size={12} /></span></strong>
+              <time title="18/11/2022 17:40" dateTime="Tuesday, 18 november 2022">
+                18/11/2022 - 17:40
+              </time>
+            </div>
+          </header>
+
+          <section className={styles.repostContent}>
+            <header className={styles.postAuthor}>
+              <Avatar
+                src="https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                avatarSideBar={3}
+              />
+              <div className={styles.postAuthorInfo}>
+                <strong>Annie Doe <span>(You)</span></strong>
+                <time title="17/11/2022 16:00" dateTime="Tuesday, 17 november 2022">
+                  17/11/2022 - 16:40
+                </time>
+              </div>
+            </header>
+
+            <div className={styles.repostContentText}>
+              content of the post
+            </div>
+          </section>
+        </article>
+
+        <article className={styles.post}>
+          <header className={styles.postAuthor}>
+            <Avatar
               src="https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-              avatarSideBar={false}
+              avatarSideBar={2}
             />
             <div className={styles.postAuthorInfo}>
               <strong>Annie Doe <span>(You)</span></strong>
