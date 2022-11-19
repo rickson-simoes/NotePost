0683709dc214870db8e34a440c3e994e534c73e1
Rickson Simoes <rickson.simoes@hotmail.com>
2022-11-19 18:21:05 -0300

    :package: UUID/ :hammer: Posts as component, also, taking values from the constants showing the full list working

--- src/Components/MainContent/index.tsx
+++ src/Components/MainContent/index.tsx
@@ -1,12 +1,95 @@
 import { Actionbar } from "../Actionbar";
 import { PostForm } from "../PostForm";
-import { Avatar } from "../Avatar";
+import { Post, QuotePost, Repost } from "../PostTypes";
 
-import { ShareNetwork, Pencil } from "phosphor-react";
-
-import styles from './MainContent.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 export function MainContent() {
+  interface IPostsListContentShared {
+    postSharedAuthor: string;
+    postSharedAvatarSrc: string;
+    postSharedDate: Date | "";
+    postSharedContent: string;
+  }
+
+  interface IPostsListContent {
+    postId: string;
+    postAuthorID: string;
+    postAuthor: string;
+    postAvatarSrc: string;
+    postType?: "Post" | "Repost" | "QuotePost";
+    postDate: Date;
+    postContent: string;
+    postShared: IPostsListContentShared;
+  };
+
+
+  const postsListContent: IPostsListContent[] = [
+    {
+      postId: uuidv4(),
+      postAuthorID: "58e4aa7f-fd4c-4d0b-9333-a6287fce6736",
+      postAuthor: "Thomas Garden",
+      postAvatarSrc: "https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      postType: "QuotePost",
+      postDate: new Date('2022-11-24 08:10:20'),
+      postContent: "This Makes sense",
+      postShared: {
+        postSharedAuthor: "Annie Doe",
+        postSharedAvatarSrc: "https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        postSharedContent: "content of the post with the whole text",
+        postSharedDate: new Date('2022-11-10 16:20:50'),
+      }
+    },
+    {
+      postId: uuidv4(),
+      postAuthorID: "58e4aa7f-fd4c-4c0b-9333-a6287fce6736",
+      postAuthor: "Lee Mel",
+      postAvatarSrc: "https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      postType: "Repost",
+      postDate: new Date('2022-11-20 23:20:50'),
+      postContent: "",
+      postShared: {
+        postSharedAuthor: "Annie Doe",
+        postSharedAvatarSrc: "https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        postSharedContent: "content of the post with the whole text",
+        postSharedDate: new Date('2022-11-10 16:20:50'),
+      }
+    },
+    {
+      postId: uuidv4(),
+      postAuthorID: "58e4aa7f-fd4c-4b0b-9333-a6287fce6736",
+      postAuthor: "Lee Mel",
+      postAvatarSrc: "https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      postType: "Post",
+      postDate: new Date('2022-11-14 12:40:50'),
+      postContent: "more content lorem ipsum",
+      postShared: {
+        postSharedAuthor: "",
+        postSharedAvatarSrc: "",
+        postSharedContent: "",
+        postSharedDate: ""
+      }
+    },
+    {
+      postId: uuidv4(),
+      postAuthorID: "58e4aa7f-fd4c-4a0b-9333-a6287fce6736",
+      postAuthor: "Annie Doe",
+      postAvatarSrc: "https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      postType: "Post",
+      postDate: new Date('2022-11-10 16:20:50'),
+      postContent: "content of the post with the whole text",
+      postShared: {
+        postSharedAuthor: "",
+        postSharedAvatarSrc: "",
+        postSharedContent: "",
+        postSharedDate: ""
+      }
+    }
+  ];
+
+  const userUUID = "58e4aa7f-fd4c-4a0b-9333-a6287fce6736";
+
+
   return (
     <div>
       <Actionbar />
@@ -14,121 +97,49 @@ export function MainContent() {
       <PostForm />
 
       <div>
-        <article className={styles.post}>
-          <header className={styles.postAuthor}>
-            <Avatar
-              src="https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-              avatarSideBar={2}
-            />
-            <div className={styles.postAuthorInfo}>
-              <div className={styles.postAuthorNameAndTag}>
-                <strong>Thomas Garden</strong>
-                <span className={styles.RepostQuotePost}> Quote Post <Pencil size={16} /></span>
-              </div>
-              <time title="19/11/2022 18:30" dateTime="Tuesday, 19 november 2022">
-                19/11/2022 - 18:30
-              </time>
-            </div>
-          </header>
-
-          <section className={styles.postContent}>
-            This makes sense
-          </section>
-
-          <section className={styles.repostContent}>
-            <header className={styles.postAuthor}>
-              <Avatar
-                src="https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-                avatarSideBar={3}
+        {postsListContent.map(value => {
+          switch (value.postType) {
+            case "Post": {
+              return <Post
+                postAuthor={value.postAuthor}
+                postAvatarSrc={value.postAvatarSrc}
+                postContent={value.postContent}
+                postDate={value.postDate}
+                isUserPrincipal={value.postAuthorID === userUUID}
+                key={value.postId}
               />
-              <div className={styles.postAuthorInfo}>
-                <div className={styles.postAuthorNameAndTag}>
-                  <strong>Annie Doe</strong>
-                  <span>&nbsp;(You)</span>
-                </div>
-                <time title="17/11/2022 16:00" dateTime="Tuesday, 17 november 2022">
-                  17/11/2022 - 16:40
-                </time>
-              </div>
-            </header>
+            }
 
-            <div className={styles.repostContentText}>
-              content of the post
-            </div>
-          </section>
-        </article>
-
-        <article className={styles.post}>
-          <header className={styles.postAuthor}>
-            <Avatar
-              src="https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-              avatarSideBar={2}
-            />
-            <div className={styles.postAuthorInfo}>
-              <div className={styles.postAuthorNameAndTag}>
-                <strong>Lee Mel</strong>
-                <span className={styles.RepostQuotePost}> Repost <ShareNetwork size={14} /></span>
-              </div>
-              <time title="18/11/2022 17:40" dateTime="Tuesday, 18 november 2022">
-                18/11/2022 - 17:40
-              </time>
-            </div>
-          </header>
-
-          <section className={styles.repostContent}>
-            <header className={styles.postAuthor}>
-              <Avatar
-                src="https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-                avatarSideBar={3}
+            case "Repost": {
+              return <Repost
+                postAuthor={value.postAuthor}
+                postAvatarSrc={value.postAvatarSrc}
+                postDate={value.postDate}
+                isUserPrincipal={value.postAuthorID === userUUID}
+                postSharedAuthor={value.postShared.postSharedAuthor}
+                postSharedAvatarSrc={value.postShared.postSharedAvatarSrc}
+                postSharedContent={value.postShared.postSharedContent}
+                postSharedDate={value.postShared.postSharedDate as Date}
+                key={value.postId}
               />
-              <div className={styles.postAuthorInfo}>
-                <div className={styles.postAuthorNameAndTag}>
-                  <strong>Annie Doe</strong>
-                  <span>(You)</span>
-                </div>
-                <time title="17/11/2022 16:00" dateTime="Tuesday, 17 november 2022">
-                  17/11/2022 - 16:40
-                </time>
-              </div>
-            </header>
+            }
 
-            <div className={styles.repostContentText}>
-              content of the post
-            </div>
-          </section>
-        </article>
-
-        <article className={styles.post}>
-          <header className={styles.postAuthor}>
-            <Avatar
-              src="https://images.unsplash.com/photo-1536416992256-1c91ce9ccdfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-              avatarSideBar={2}
-            />
-            <div className={styles.postAuthorInfo}>
-              <div className={styles.postAuthorNameAndTag}>
-                <strong>Annie Doe</strong>
-                <span>(You)</span>
-              </div>
-              <time title="17/11/2022 16:00" dateTime="Tuesday, 17 november 2022">
-                17/11/2022 - 16:40
-              </time>
-            </div>
-          </header>
-
-          <section className={styles.postContent}>
-            content of the post
-          </section>
-
-          <footer className={styles.postReactionBox}>
-            <button>
-              <Pencil />
-            </button>
-
-            <button>
-              <ShareNetwork />
-            </button>
-          </footer>
-        </article>
+            case "QuotePost": {
+              return <Repost
+                postAuthor={value.postAuthor}
+                postAvatarSrc={value.postAvatarSrc}
+                postContent={value.postContent}
+                postDate={value.postDate}
+                isUserPrincipal={value.postAuthorID === userUUID}
+                postSharedAuthor={value.postShared.postSharedAuthor}
+                postSharedAvatarSrc={value.postShared.postSharedAvatarSrc}
+                postSharedContent={value.postShared.postSharedContent}
+                postSharedDate={value.postShared.postSharedDate as Date}
+                key={value.postId}
+              />
+            }
+          }
+        })}
       </div>
 
     </div>
---
+++ src/Components/PostTypes/index.tsx
@@ -0,0 +1,162 @@
+import { Pencil, ShareNetwork } from "phosphor-react";
+import { v4 as uuidv4 } from 'uuid';
+import { format, formatDistanceToNow } from 'date-fns';
+
+import { Avatar } from "../Avatar";
+
+import styles from './PostsTypes.module.css';
+
+interface IPostContent {
+  postAuthor: string;
+  postAvatarSrc: string;
+  postDate: Date;
+  postContent?: string;
+  isUserPrincipal: boolean;
+}
+
+interface IRepostAndQuoteContent extends IPostContent {
+  postSharedAuthor: string;
+  postSharedAvatarSrc: string;
+  postSharedDate: Date;
+  postSharedContent: string;
+}
+
+function FormatDate(date: Date) {
+  return format(date, "d 'of' LLLL',' HH:mm");
+};
+
+export function Post({ postAuthor, postAvatarSrc, postDate, postContent, isUserPrincipal }: IPostContent) {
+  const postDateFormatted = FormatDate(postDate);
+
+  return (
+    <article className={styles.post}>
+      <header className={styles.postAuthor}>
+        <Avatar
+          src={postAvatarSrc}
+          avatarSideBar={2}
+        />
+        <div className={styles.postAuthorInfo}>
+          <div className={styles.postAuthorNameAndTag}>
+            <strong>{postAuthor}</strong>
+            <span>{isUserPrincipal && "(You)"}</span>
+          </div>
+          <time title={postDateFormatted} dateTime={postDateFormatted}>
+            {postDateFormatted}
+          </time>
+        </div>
+      </header>
+
+      <section className={styles.postContent}>
+        {postContent}
+      </section>
+
+      <footer className={styles.postReactionBox}>
+        <button>
+          <Pencil />
+        </button>
+
+        <button>
+          <ShareNetwork />
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export function Repost(props: IRepostAndQuoteContent) {
+  const postDateFormatted = FormatDate(props.postDate);
+  const postSharedDateFormatted = FormatDate(props.postSharedDate);
+
+  return (
+    <article className={styles.post}>
+      <header className={styles.postAuthor}>
+        <Avatar
+          src={props.postAvatarSrc}
+          avatarSideBar={2}
+        />
+        <div className={styles.postAuthorInfo}>
+          <div className={styles.postAuthorNameAndTag}>
+            <strong>{props.postAuthor}</strong>
+            <span className={styles.RepostQuotePost}> Repost <ShareNetwork size={14} /></span>
+          </div>
+          <time title={postDateFormatted} dateTime={postDateFormatted}>
+            {postDateFormatted}
+          </time>
+        </div>
+      </header>
+
+      <section className={styles.repostContent}>
+        <header className={styles.postAuthor}>
+          <Avatar
+            src={props.postSharedAvatarSrc}
+            avatarSideBar={3}
+          />
+          <div className={styles.postAuthorInfo}>
+            <div className={styles.postAuthorNameAndTag}>
+              <strong>{props.postSharedAuthor}</strong>
+              <span>{props.isUserPrincipal && "(You)"}</span>
+            </div>
+            <time title={postSharedDateFormatted} dateTime={postSharedDateFormatted}>
+              {postSharedDateFormatted}
+            </time>
+          </div>
+        </header>
+
+        <div className={styles.repostContentText}>
+          {props.postSharedContent}
+        </div>
+      </section>
+    </article>
+  )
+}
+
+export function QuotePost(props: IRepostAndQuoteContent) {
+  const postDateFormatted = FormatDate(props.postDate);
+  const postSharedDateFormatted = FormatDate(props.postSharedDate);
+
+  return (
+    <article className={styles.post}>
+      <header className={styles.postAuthor}>
+        <Avatar
+          src={props.postAvatarSrc}
+          avatarSideBar={2}
+        />
+        <div className={styles.postAuthorInfo}>
+          <div className={styles.postAuthorNameAndTag}>
+            <strong>{props.postAuthor}</strong>
+            <span className={styles.RepostQuotePost}> Quote Post <Pencil size={16} /></span>
+          </div>
+          <time title={postDateFormatted} dateTime={postDateFormatted}>
+            {postDateFormatted}
+          </time>
+        </div>
+      </header>
+
+      <section className={styles.postContent}>
+        {props.postContent}
+      </section>
+
+      <section className={styles.repostContent}>
+        <header className={styles.postAuthor}>
+          <Avatar
+            src={props.postSharedAvatarSrc}
+            avatarSideBar={3}
+          />
+          <div className={styles.postAuthorInfo}>
+            <div className={styles.postAuthorNameAndTag}>
+              <strong>{props.postSharedAuthor}</strong>
+              <span>{props.isUserPrincipal && "(You)"}</span>
+            </div>
+            <time title={postSharedDateFormatted} dateTime={postSharedDateFormatted}>
+              {postSharedDateFormatted}
+            </time>
+          </div>
+        </header>
+
+        <div className={styles.repostContentText}>
+          {props.postSharedContent}
+        </div>
+      </section>
+    </article>
+  )
+}
