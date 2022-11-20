3ee3ff5c1882c89a537811dabb96736fe28f0bdc
Rickson Simoes <rickson.simoes@hotmail.com>
2022-11-19 22:59:54 -0300

    :folder: LocalStorageContents update folder/ :hammer: Date better displayed and new post from main user being saved into localstorage and updating the list

--- src/Components/MainContent/index.tsx
+++ src/Components/MainContent/index.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import { format } from 'date-fns';
 
 import { Actionbar } from "../Actionbar";
 import { PostForm } from "../PostForm";
 import { Post, QuotePost, Repost } from "../PostTypes";
+
+import { v4 as uuidv4 } from 'uuid';
 
 interface IPostsListContentShared {
   postSharedAuthorID: string;
@@ -23,23 +26,58 @@ interface IPostsListContent {
   postShared: IPostsListContentShared;
 };
 
+interface IMainUserInformation {
+  id: string;
+  name: string;
+  avatar: string;
+  bio: string;
+  follows: number;
+  followers: number;
+}
+
 export function MainContent() {
   const [postListContent, setPostListContent] = useState<IPostsListContent[]>([]);
-  const [principalUserUUID, setPrincipalUserUUID] = useState("");
+  const [principalUser, setPrincipalUser] = useState<IMainUserInformation>();
 
   useEffect(() => {
     const getLocalStoragePostList = JSON.parse(localStorage.getItem("@Posterr:PostList") as string);
-    const getLocalStorageMainUserUUID = JSON.parse(localStorage.getItem("@Posterr:UserUUID") as string);
+    const getLocalStorageMainUser = JSON.parse(localStorage.getItem("@Poster:MainUserInformation") as string);
 
     setPostListContent(getLocalStoragePostList);
-    setPrincipalUserUUID(getLocalStorageMainUserUUID);
+    setPrincipalUser(getLocalStorageMainUser);
   }, []);
+
+  function handleSubmitNewPost(text: string) {
+    const newPostToInsert: IPostsListContent = {
+      postId: uuidv4(),
+      postAuthorID: principalUser?.id!,
+      postAuthor: principalUser?.name!,
+      postAvatarSrc: principalUser?.avatar!,
+      postContent: text,
+      postDate: format(new Date(Date.now()), "yyyy-LL-dd HH:mm:ss"),
+      postShared: {
+        postSharedAuthor: "",
+        postSharedAuthorID: "",
+        postSharedAvatarSrc: "",
+        postSharedContent: "",
+        postSharedDate: ""
+      },
+      postType: "Post",
+    };
+
+    setPostListContent(() => {
+      const newPostListValue = [newPostToInsert, ...postListContent];
+      localStorage.setItem("@Posterr:PostList", JSON.stringify(newPostListValue));
+
+      return [newPostToInsert, ...postListContent];
+    });
+  }
 
   return (
     <div>
       <Actionbar />
 
-      <PostForm />
+      <PostForm onSubmitNewPost={handleSubmitNewPost} />
 
       <div>
         {postListContent.map(value => {
@@ -50,7 +88,7 @@ export function MainContent() {
                 postAvatarSrc={value.postAvatarSrc}
                 postContent={value.postContent}
                 postDate={value.postDate}
-                isUserPrincipal={value.postAuthorID === principalUserUUID}
+                isUserPrincipal={value.postAuthorID === principalUser?.id}
                 key={value.postId}
               />
             }
@@ -60,7 +98,7 @@ export function MainContent() {
                 postAuthor={value.postAuthor}
                 postAvatarSrc={value.postAvatarSrc}
                 postDate={value.postDate}
-                isUserPrincipal={value.postShared.postSharedAuthorID === principalUserUUID}
+                isUserPrincipal={value.postShared.postSharedAuthorID === principalUser?.id}
                 postSharedAuthor={value.postShared.postSharedAuthor}
                 postSharedAvatarSrc={value.postShared.postSharedAvatarSrc}
                 postSharedContent={value.postShared.postSharedContent}
@@ -75,7 +113,7 @@ export function MainContent() {
                 postAvatarSrc={value.postAvatarSrc}
                 postContent={value.postContent}
                 postDate={value.postDate}
-                isUserPrincipal={value.postShared.postSharedAuthorID === principalUserUUID}
+                isUserPrincipal={value.postShared.postSharedAuthorID === principalUser?.id}
                 postSharedAuthor={value.postShared.postSharedAuthor}
                 postSharedAvatarSrc={value.postShared.postSharedAvatarSrc}
                 postSharedContent={value.postShared.postSharedContent}
--- src/Components/PostForm/index.tsx
+++ src/Components/PostForm/index.tsx
@@ -1,14 +1,43 @@
 import { PencilLine } from "phosphor-react";
 
+import { ChangeEvent, FormEvent, useState } from "react";
+
 import styles from './PostForm.module.css';
 
-export function PostForm() {
+interface IPostForm {
+  onSubmitNewPost: (text: string) => void;
+}
+
+export function PostForm({ onSubmitNewPost }: IPostForm) {
+  const [textAreaPost, setTextAreaPost] = useState("");
+
+  function handlePostText(event: ChangeEvent<HTMLTextAreaElement>) {
+    setTextAreaPost((state) => {
+      return state = event.target.value;
+    });
+  }
+
+  function handleNewPost(event: FormEvent) {
+    event.preventDefault();
+    onSubmitNewPost(textAreaPost);
+    setTextAreaPost("");
+  }
+
   return (
-    <form className={styles.postForm}>
-      <textarea placeholder="Post something about your day =)"></textarea>
+    <form onSubmit={handleNewPost} className={styles.postForm}>
+      <textarea
+        placeholder="Post something about your day =)"
+        onChange={handlePostText}
+        value={textAreaPost}
+        required
+      />
 
       <footer>
-        <button type="submit">Post </button>
+        <button
+          type="submit"
+        >
+          Post
+        </button>
 
         <div><PencilLine size={20} />00/777</div>
       </footer>
