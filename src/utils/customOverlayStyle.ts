6cf8a4893387ed87984b7990a3631e49697f5c0c
Rickson <rickson.simoes@hotmail.com>
2023-03-31 20:34:37 -0300

    :hammer: Customer Overlay Modal style

---
+++ src/utils/customOverlayStyle.ts
@@ -0,0 +1,15 @@
+export const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: "#0e0e0e",
+    border: "0"
+  },
+  overlay: {
+    backgroundColor: "rgb(0 0 0 / 68%)"
+  }
+};
