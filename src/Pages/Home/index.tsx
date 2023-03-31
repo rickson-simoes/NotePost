02cdd727ad547f9cb021646f9946532ea19776fb
Rickson <rickson.simoes@hotmail.com>
2023-03-31 17:53:17 -0300

    :fire: MainContent Component removal and more code reduce inside Home Page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,54 @@
+import { useContext, useState } from 'react';
+import { format } from 'date-fns';
 import styles from './Home.module.css';
+import { AppManagementContext } from '../../Context/AppManagementContext';
+import { IPostsListContent, IQuotePostContent } from '../../@Types';
+import { Post, QuotePost, Repost } from '../../Components/PostTypes';
 import { Header } from '../../Components/Header/Header';
 import { Sidebar } from '../../Components/Sidebar';
-import { MainContent } from '../../Components/MainContent';
+import { PostForm } from '../../Components/PostForm';
 
 export function Home() {
+  const [postListContent, setPostListContent] = useState<IPostsListContent[]>(JSON.parse(localStorage.getItem("@NotePost:PostList")!));
+  const { mainUserInfo } = useContext(AppManagementContext);
+
+  function submitContentToPostList(postTextContent: string, postType: "QuotePost" | "Post" | "Repost", postListQuoteContent?: IQuotePostContent) {
+
+    const newPostToInsert: IPostsListContent = {
+      postId: crypto.randomUUID(),
+      postAuthorID: mainUserInfo.id,
+      postAuthor: mainUserInfo.name,
+      postAvatarSrc: mainUserInfo.avatar!,
+      postContent: postTextContent,
+      postDate: format(new Date(Date.now()), "yyyy-LL-dd HH:mm:ss"),
+      postShared: {
+        postSharedAuthor: postListQuoteContent?.postSharedAuthor || "",
+        postSharedAuthorID: postListQuoteContent?.postSharedAuthorID || "",
+        postSharedAvatarSrc: postListQuoteContent?.postSharedAvatarSrc || "",
+        postSharedContent: postListQuoteContent?.postSharedContent || "",
+        postSharedDate: postListQuoteContent?.postSharedDate || ""
+      },
+      postType: postType,
+    };
+
+    const newPostListValue = [newPostToInsert, ...postListContent];
+
+    setPostListContent(newPostListValue);
+
+    localStorage.setItem("@NotePost:PostList", JSON.stringify(newPostListValue));
+  }
+
+  function handleSubmitNewPost(text: string) {
+    submitContentToPostList(text, "Post");
+  }
+
+  function handleQuotePostSubmitContent(props: IQuotePostContent) {
+    submitContentToPostList(props.postContent, "QuotePost", props);
+  }
+  function handleRepostSubmitContent(props: IQuotePostContent) {
+    submitContentToPostList(props.postContent = "", "Repost", props);
+  }
+
   return (
     <>
       <Header />
@@ -11,7 +56,45 @@ export function Home() {
       <main className={styles.wrapContent}>
         <Sidebar />
 
-        <MainContent />
+        <div>
+          {/* <Actionbar onToggleChange={handleToggle} /> */}
+
+          <PostForm onSubmitNewPost={handleSubmitNewPost} isUserAllowedToPost={true} />
+
+          <div className={styles.mainContent}>
+            {postListContent.map(post => {
+              switch (post.postType) {
+                case "Post": {
+                  return <Post
+                    {...post}
+                    isUserPrincipal={post.postAuthorID === mainUserInfo.id}
+                    onSubmitQuotePost={handleQuotePostSubmitContent}
+                    onSubmitRepost={handleRepostSubmitContent}
+                    key={post.postId}
+                  />
+                }
+
+                case "Repost": {
+                  return <Repost
+                    {...post}
+                    {...post.postShared}
+                    isUserPrincipal={post.postShared.postSharedAuthorID === mainUserInfo.id}
+                    key={post.postId}
+                  />
+                }
+
+                case "QuotePost": {
+                  return <QuotePost
+                    {...post}
+                    {...post.postShared}
+                    isUserPrincipal={post.postShared.postSharedAuthorID === mainUserInfo.id}
+                    key={post.postId}
+                  />
+                }
+              }
+            })}
+          </div>
+        </div>
       </main>
     </>
   )
