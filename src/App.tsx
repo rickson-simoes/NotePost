c509867c5717f76d15f8dbcb699545d9eb711aae
Rickson <rickson.simoes@hotmail.com>
2023-03-31 16:39:06 -0300

    :hammer: App: Use of AppManagementContextProvider wrapping Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
 import './global.css';
+import { AppManagementContextProvider } from './Context/AppManagementContext';
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <AppManagementContextProvider>
+        <Router />
+      </AppManagementContextProvider>
     </BrowserRouter>
   )
 }
