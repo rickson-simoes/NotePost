7031a3efa90ad2127bd5635be17cd718445132ec
Rickson <rickson.simoes@hotmail.com>
2023-03-31 16:37:29 -0300

    :hammer: Using context to return all data from main user

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,34 +1,27 @@
 import { Gear } from 'phosphor-react';
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Avatar } from '../Avatar';
 
 import styles from './Sidebar.module.css';
 
-import { IUserInformation } from "../../@Types";
 import { Link } from 'react-router-dom';
+import { AppManagementContext } from '../../Context/AppManagementContext';
 
 export function Sidebar() {
-  const [userInformation, setUserInformation] = useState<IUserInformation>();
-
-  useEffect(() => {
-    const getFromlocalStorage = JSON.parse(localStorage.getItem("@NotePost:MainUserInformation")!);
-
-    setUserInformation(getFromlocalStorage);
-  }, []);
+  const { mainUserInfo } = useContext(AppManagementContext);
 
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.backgroundUserImage} src={userInformation?.backgroundAvatar} alt="Cover image from user" />
+      <img className={styles.backgroundUserImage} src={mainUserInfo.backgroundAvatar} alt="Cover image from user" />
 
       <div className={styles.sidebarUserData}>
-        <Avatar src={userInformation?.avatar} />
-        <strong>{userInformation?.name}</strong>
-        <span>{userInformation?.bio}</span>
+        <Avatar src={mainUserInfo.avatar} />
+        <strong>{mainUserInfo.name}</strong>
+        <span>{mainUserInfo.bio}</span>
       </div>
 
       <footer>
-        {/* @WORKAROUND - GET MAIN USER ID */}
-        <Link to="/user/58e4aa7f-fd4c-4a0b-9333-a6287fce6736">
+        <Link to={`/user/${mainUserInfo.id}`}>
           <Gear size={40} />
         </Link>
       </footer>
