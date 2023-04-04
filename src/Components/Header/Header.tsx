e5f5bbaf5f27b209c5eb35cf51094a2e69f896a5
Rickson <rickson.simoes@hotmail.com>
2023-04-04 11:16:33 -0300

    :art: :star: Css Changes into the SVG and IMG Sample

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <Link to="/">
-        <NotePencil size={40} /> Note<span>Post</span>
+        <NotePencil size={40} weight="duotone" /> Note<span>Post</span>
       </Link>
     </header>
   )
