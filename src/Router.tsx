08b4d110d087feb30f45cdb5f8af1dd6663ab3ba
Rickson <rickson.simoes@hotmail.com>
2023-04-04 09:26:48 -0300

    :hammer: Redirection of user if url is not correct

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { User } from './Pages/User'
+import { useContext } from 'react';
+import { AppManagementContext } from './Context/AppManagementContext';
 
 export function Router() {
+  const { mainUserInfo } = useContext(AppManagementContext);
+
+  if (window.location.pathname == "/user") {
+    Navigate({ to: `/user/${mainUserInfo.id}` });
+  }
+
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/user/:id" element={<User />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
