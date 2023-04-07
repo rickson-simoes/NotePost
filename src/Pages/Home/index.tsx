e216caed5650e0d3d725f6654891c8ad791206ad
Rickson <rickson.simoes@hotmail.com>
2023-04-07 12:03:45 -0300

    :wrench: Toggle Bar - Filters who you follow to see the posts

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,40 @@
+import { useContext, useEffect, useState } from 'react';
+
 import styles from './Home.module.css';
+
+import { IPostsListContent, Toggle } from "../../@Types";
 import { Header } from '../../Components/Header/Header';
 import { Sidebar } from '../../Components/Sidebar';
 import { SubmitContent } from '../../Components/SubmitContent/SubmitContent';
+import { Actionbar } from '../../Components/Actionbar';
+import { AppManagementContext } from '../../Context/AppManagementContext';
 
 export function Home() {
+  const { postList, mainUserInfo } = useContext(AppManagementContext);
+  const deepClonePostList = JSON.parse(JSON.stringify(postList));
+  const [postListFiltered, setPostListFiltered] = useState<IPostsListContent[]>(deepClonePostList)
+
+  function handleToggleChange(value?: Toggle) {
+    if (value == Toggle.following) {
+      setPostListFiltered([]);
+      const userIds: string[] = [];
+      mainUserInfo.follows.filter(user => userIds.push(user.id));
+
+      for (const userID of userIds) {
+        const findUsersPostsWhoIFollow = postList.filter(post => post.authorID == userID);
+        setPostListFiltered((state) => {
+          return [...findUsersPostsWhoIFollow, ...state]
+        });
+      }
+    } else {
+      setPostListFiltered(deepClonePostList);
+    }
+  }
+
+  useEffect(() => {
+    handleToggleChange(localStorage.getItem("@NotePost:MainContentToggle") as Toggle)
+  }, [])
+
   return (
     <>
       <Header />
@@ -11,7 +42,12 @@ export function Home() {
       <main className={styles.wrapContent}>
         <Sidebar />
 
-        <SubmitContent />
+
+        <section>
+          <Actionbar onToggleChange={handleToggleChange} />
+
+          <SubmitContent postList={postListFiltered} />
+        </section>
       </main>
     </>
   )
